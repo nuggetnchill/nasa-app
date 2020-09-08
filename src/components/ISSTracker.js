dc37d594335c35226f7d1b62cd71452455a227c6
Justin Nguyen <nuggetnchill@gmail.com>
2020-09-08 17:05:31 -0500

    added leaflet.js map and iss tracking function

--- src/components/ISSTracker.js
+++ src/components/ISSTracker.js
@@ -1,23 +1,44 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { useEffect, useState } from "react";
+import { Map, TileLayer, Marker } from "react-leaflet";
+import { Icon } from "leaflet";
+import issLogo from "../assets/iss.png";
+
+const issIcon = new Icon({
+  iconUrl: issLogo,
+  iconSize: [200, 100],
+  iconAnchor: [100, 50],
+});
 
 const ISSTracker = () => {
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
+  const [latitude, setLatitude] = useState(37.6872);
+  const [longitude, setLongitude] = useState(-97.3301);
 
-  const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
-  const getISS = async () => {
-    const response = await fetch(api_url);
-    const data = await response.json();
-    const { latitude, longitude } = data;
-    setLongitude(longitude);
-    setLatitude(latitude);
-  };
-  getISS();
+  useEffect(() => {
+    const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
+    const getISS = async () => {
+      const response = await fetch(api_url);
+      const data = await response.json();
+      const { latitude, longitude } = data;
+      setLongitude(longitude);
+      setLatitude(latitude);
+    };
+
+    getISS();
+  }, []);
 
   return (
     <div>
+      <div className='leaflet-container'>
+        <Map center={[latitude, longitude]} zoom={3}>
+          <TileLayer
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker icon={issIcon} position={[latitude, longitude]} />
+        </Map>
+      </div>
       <h1>{latitude}</h1>
       <h1>{longitude}</h1>
     </div>
